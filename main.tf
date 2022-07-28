b70e17565f306339c99c48662f0eb77e318e4b1a
Ismael Doblas <85618332+IsmaelDoblas@users.noreply.github.com>
2022-07-28 21:01:38 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,33 @@
+provider "azurerm" {
+  features {}
+}
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+}
+
+data "azurerm_resource_group" "example" {
+  name     = "Devops"
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "vnetismael"
+  location            = data.azurerm_resource_group.example.location
+  resource_group_name = data.azurerm_resource_group.example.name
+  address_space       = ["10.0.0.0/16"]
+  dns_servers         = ["10.0.0.4", "10.0.0.5"]
+
+  subnet {
+    name           = "subnet1"
+    address_prefix = "10.0.1.0/24"
+  }
+
+  tags = {
+    environment = "Production"
+  }
+}
